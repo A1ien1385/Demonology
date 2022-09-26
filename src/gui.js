5d5bf5b167ba4a06fc732a358557f88d431ddea5
A1ien1385 <damian.zawadzki@tlen.pl>
2022-09-26 20:14:55 +0200

    day 1 done

--- src/gui.js
+++ src/gui.js
@@ -1,7 +1,7 @@
 import React, {useRef, useEffect} from "react";
 import { ReactComponent as Scene } from "./assets/BackgroundPage.svg";
 import gsap from "gsap";
-import { TweenMax } from "gsap/gsap-core";
+
 
 export default function Gui() {
   const wrapper = useRef(null);
