98e6a2c5e935010c3df647b5e72d77f416358d43
A1ien1385 <damian.zawadzki@tlen.pl>
2022-09-26 15:54:37 +0200

    starting animate

--- src/gui.js
+++ src/gui.js
@@ -7,8 +7,19 @@ export default function Gui() {
   const wrapper = useRef(null);
 
   useEffect(()=> {
+    const [elements] = wrapper.current.children;
+
+    const UpperRunes = elements.getElementById('otherRUnes');
+    const DownRunes = elements.getElementById('otherRunes2');
+    const pentagram = elements.getElementById('pentagram');
     
-      
+    
+    gsap.set([DownRunes, UpperRunes], {autoAlpha: .3, ease: "power3.inout"});
+    gsap.set([pentagram], {y: -20, scale: .9});
+
+    gsap.to(UpperRunes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
+    gsap.to(DownRunes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
+    gsap.to(pentagram, {rotate: 360, ease: "none", transformOrigin:"center", repeat: -1, duration: 7,});
       
   })
 
