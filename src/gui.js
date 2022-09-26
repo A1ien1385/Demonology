aa8a4f3de8509ac5c1a9a63ff5cf2550ec01030b
A1ien1385 <damian.zawadzki@tlen.pl>
2022-09-26 19:47:19 +0200

    main interface animation done

--- src/gui.js
+++ src/gui.js
@@ -12,14 +12,24 @@ export default function Gui() {
     const UpperRunes = elements.getElementById('otherRUnes');
     const DownRunes = elements.getElementById('otherRunes2');
     const pentagram = elements.getElementById('pentagram');
+    const circles = elements.getElementById('Pengtagram_x5F_circle');
+    const title = elements.getElementById('title');
     
     
-    gsap.set([DownRunes, UpperRunes], {autoAlpha: .3, ease: "power3.inout"});
-    gsap.set([pentagram], {y: -20, scale: .9});
+    gsap.set([DownRunes, UpperRunes, pentagram], {autoAlpha: .3, ease: "power3.inout"});
+    gsap.set([title, UpperRunes, DownRunes], {autoAlpha: .1, x: -550, ease: "power3.inout"});
+    
 
     gsap.to(UpperRunes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
+    gsap.to(UpperRunes, {duration: 1.7, x: 0});
     gsap.to(DownRunes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
-    gsap.to(pentagram, {rotate: 360, ease: "none", transformOrigin:"center", repeat: -1, duration: 7,});
+    gsap.to(DownRunes, {duration: 1.7, x: 0});
+    gsap.to(pentagram, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
+    gsap.to(circles, {duration: 15, ease: "none", rotate: 360, transformOrigin: "center", repeat: -1});
+    gsap.to(title, {duration: 1.7, x: 20});
+    gsap.to(title, {duration: 2, autoAlpha: 1, repeat: -1, yoyo: true})
+
+   
       
   })
 
