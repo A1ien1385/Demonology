0446b9f9d543bb55bb647b2874d7004cef3509c8
A1ien1385 <damian.zawadzki@tlen.pl>
2022-09-26 22:36:46 +0200

    day 1 done 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import React, {useRef, useEffect} from "react";
 import Gui from "./gui";
-import gsap from "gsap";
-import { TweenMax } from "gsap/gsap-core";
 import "./style/main.scss";
 
 function App() {
