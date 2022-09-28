d84d1ce33db42803e262d4c7f2e25bb23ed207ec
A1ien1385 <damian.zawadzki@tlen.pl>
2022-09-28 21:31:13 +0200

    post scriptum done

--- src/components/praembulum.jsx
+++ src/components/praembulum.jsx
@@ -10,7 +10,7 @@ function Praeambulum(props) {
     return (
       <>
       <div className="page_container--horizon">
-        <h1 className="page_title">Praembulum</h1>
+        <h1 className="page_title">Praeambulum</h1>
         <main id="succub"  onClick={toggleClass} className={isActive ? 'activeArt' : null}>
         </main>
         <div className="page_description_content"><h5 className="page_desc_content_txt">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero placeat voluptas aliquid explicabo id debitis similique officiis quod architecto suscipit. Expedita animi saepe aspernatur et autem earum quis dolore adipisci!
