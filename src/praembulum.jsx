48856db7b5e0ae1dc2ff46ea8f0246bcac6413f3
A1ien1385 <damian.zawadzki@tlen.pl>
2022-09-28 11:13:42 +0200

    Add popup start

--- src/praembulum.jsx
+++ src/praembulum.jsx
@@ -1,10 +1,21 @@
 import Nav from "./nav";
+import { useState } from "react";
 
-function Praeambulum() {
+function Praeambulum(props) {
+  const [isActive, setActive] = useState(false);
+  function toggleClass()
+  {
+   setActive(!isActive);
+  };
+
     return (
-      <>
+      
+      <div className="page_container--horizon">
         <h1 className="page_title">Praembulum</h1>
-      </>
+        <main  onClick={toggleClass} className={isActive ? 'activeArt' : null}>
+        </main>
+      </div> 
+      
     );
   }
   
