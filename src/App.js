bf64917f38349e1451219e074a29ef52e83da036
A1ien1385 <damian.zawadzki@tlen.pl>
2022-09-28 08:20:05 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./style/main.scss";
 import Nav from "./nav";
 import Intro from "./intro";
+import Praeambulum from "./praembulum";
 
 function App() {
+
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <Intro/>;
+      break;
+   
+    case "/praeambulum":
+      component = <Praeambulum />;
+      break;
+
+  }
   
   return (
     <div className="App">
       
-      <Intro />
+      { component }
       <Nav/>
     
     </div>
---
+++ src/praembulum.jsx
@@ -0,0 +1,11 @@
+import Nav from "./nav";
+
+function Praeambulum() {
+    return (
+      <>
+        <h1 className="page_title">Praembulum</h1>
+      </>
+    );
+  }
+  
+  export default Praeambulum;
