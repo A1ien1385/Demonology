e2be54361e057936737adfb70f0e6cdeec6e5f82
A1ien1385 <damian.zawadzki@tlen.pl>
2022-09-28 14:47:38 +0200

    Praembulum

--- src/praembulum.jsx
+++ src/praembulum.jsx
@@ -9,13 +9,17 @@ function Praeambulum(props) {
   };
 
     return (
-      
+      <>
       <div className="page_container--horizon">
         <h1 className="page_title">Praembulum</h1>
         <main  onClick={toggleClass} className={isActive ? 'activeArt' : null}>
         </main>
+        <div className="page_description_content"><h5>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero placeat voluptas aliquid explicabo id debitis similique officiis quod architecto suscipit. Expedita animi saepe aspernatur et autem earum quis dolore adipisci!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero placeat voluptas aliquid explicabo id debitis similique officiis quod architecto suscipit. Expedita animi saepe aspernatur et autem earum quis dolore adipisci!
+          </h5></div>
       </div> 
       
+      </>
     );
   }
   
