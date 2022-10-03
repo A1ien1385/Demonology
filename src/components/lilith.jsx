ccfe5296dc84303820326f7651590e24aa1ce435
A1ien1385 <damian.zawadzki@tlen.pl>
2022-10-03 18:36:43 +0200

    improve rwd 2

--- src/components/lilith.jsx
+++ src/components/lilith.jsx
@@ -13,7 +13,7 @@ function Lilith(props) {
           <h1 className="page_title">Lilith</h1>
           <main id="lilith"  onClick={toggleClass} className={isActive ? 'activeArt' : null}>
           </main>
-          <button><a className="showMore" href="/lilithTwo">Show more</a></button>
+          <a className="showMore" href="/lilithTwo">Show more</a>
           <div className="page_description_content"><h5 className="page_desc_content_txt">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero placeat voluptas aliquid explicabo id debitis similique officiis quod architecto suscipit. Expedita animi saepe aspernatur et autem earum quis dolore adipisci!
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero placeat voluptas aliquid explicabo id debitis similique officiis quod architecto suscipit. Expedita animi saepe aspernatur et autem earum quis dolore adipisci!
             </h5></div>
