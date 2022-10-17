78a690122b7a577fc0c263fcf529e560a7b5bf62
A1ien1385 <damian.zawadzki@tlen.pl>
2022-10-17 19:49:18 +0200

    bg correct

--- src/components/intro.js
+++ src/components/intro.js
@@ -9,20 +9,20 @@ export default function Intro() {
   useEffect(()=> {
     const [elements] = wrapper.current.children;
 
-    const UpperRunes = elements.getElementById('otherRUnes');
+    // const UpperRunes = elements.getElementById('otherRUnes');
     // const DownRunes = elements.getElementById('otherRunes2');
     const pentagram = elements.getElementById('pentagram');
     const circles = elements.getElementById('Pengtagram_x5F_circle');
     const title = elements.getElementById('title_2_');
     
     
-    gsap.set([UpperRunes, pentagram], {autoAlpha: .3, ease: "power3.inout"});
+    // gsap.set([UpperRunes, pentagram], {autoAlpha: .3, ease: "power3.inout"});
     gsap.set([title], {autoAlpha: .1, x: -550, ease: "power3.inout"});
-    gsap.set([UpperRunes], {autoAlpha: .1, x: 550, ease: "power3.inout"});
+    // gsap.set([UpperRunes], {autoAlpha: .1, x: 550, ease: "power3.inout"});
     
 
-    gsap.to(UpperRunes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
-    gsap.to(UpperRunes, {duration: 1.7, x: 0});
+    // gsap.to(UpperRunes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
+    // gsap.to(UpperRunes, {duration: 1.7, x: 0});
     // gsap.to(DownRunes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
     // gsap.to(DownRunes, {duration: 1.7, x: 0});
     gsap.to(pentagram, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
