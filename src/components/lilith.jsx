c8eb2ec7fbc31d04b7becd1c444aa60ed2d333a1
A1ien1385 <damian.zawadzki@tlen.pl>
2022-10-30 23:37:37 +0100

    add succubus description

--- src/components/lilith.jsx
+++ src/components/lilith.jsx
@@ -15,7 +15,7 @@ function Lilith(props) {
           <main id="lilith"  onClick={toggleClass} className={isActive ? 'activeArt' : null}>
           </main>
           <a className="showMore" href="/lilithTwo">Lilith art 2</a>
-          <a className="showMore" href="/succubus">Succubus</a>
+          <a className="showMore" href="/succubus">Succubus art 3</a>
           <div className="page_description_content"><h5 className="page_desc_content_txt">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero placeat voluptas aliquid explicabo id debitis similique officiis quod architecto suscipit. Expedita animi saepe aspernatur et autem earum quis dolore adipisci!
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero placeat voluptas aliquid explicabo id debitis similique officiis quod architecto suscipit. Expedita animi saepe aspernatur et autem earum quis dolore adipisci!
             </h5></div>
