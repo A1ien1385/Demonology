a0e7013f6e3160ceeee2aa5433a8ddb7b72a1de4
A1ien1385 <damian.zawadzki@tlen.pl>
2022-12-06 21:42:00 +0100

    change intro page

--- src/components/intro.js
+++ src/components/intro.js
@@ -14,16 +14,18 @@ export default function Intro() {
     const pentagram = elements.getElementById('pentagram');
     const circles = elements.getElementById('Pengtagram_x5F_circle');
     const title = elements.getElementById('title_2_');
+    const eyes = elements.getElementById('eyes');
     
     
     // gsap.set([UpperRunes, pentagram], {autoAlpha: .3, ease: "power3.inout"});
+    gsap.set(eyes, {autoAlpha: 0});
     gsap.set([title], {autoAlpha: .1, x: -550, ease: "power3.inout"});
     // gsap.set([UpperRunes], {autoAlpha: .1, x: 550, ease: "power3.inout"});
     
 
     // gsap.to(UpperRunes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
     // gsap.to(UpperRunes, {duration: 1.7, x: 0});
-    // gsap.to(DownRunes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
+    gsap.to(eyes, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
     // gsap.to(DownRunes, {duration: 1.7, x: 0});
     gsap.to(pentagram, {duration: 2, ease: "power1.out", repeat: -1, yoyo: true, autoAlpha: 1});
     gsap.to(circles, {duration: 15, ease: "none", rotate: 360, transformOrigin: "center", repeat: -1});
